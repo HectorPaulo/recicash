3a90eaf67bb072f64b799d45bd678ad8508e2450
Hector Paulo <adrian.paulo.vaz@gmail.com>
2025-04-08 10:09:20 -0600

    create repo

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-gray-900 to-white">
+      <p className="text-8xl font-black animate-pulse text-gray-800 hover:scale-125 cursor-all-scroll">Hola mundo</p>
+    </div>
+  )
+}
+
+export default App
